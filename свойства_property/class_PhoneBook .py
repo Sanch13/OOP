470a55650019eb56fe185b3e6e2b5c785fd1b881
Aliaksandr Zubchyk <Sanch13sanch@gmail.com>
2022-08-03 17:03:48 +0300

    class_Phonebook

---
+++ свойства_property/class_PhoneBook .py
@@ -0,0 +1,28 @@
+class PhoneNumber:
+    def __init__(self, number, fio):
+        self.number = number
+        self.fio = fio
+
+
+class PhoneBook:
+    def __init__(self):
+        self.phones = []
+
+    def add_phone(self, phone):
+        self.phones.append(phone)
+
+    def remove_phone(self, indx):
+        self.phones.pop(indx)
+
+    def get_phone_list(self):
+        return self.phones
+
+
+
+
+
+p = PhoneBook()
+p.add_phone(PhoneNumber(12345678901, "Сергей Балакирев"))
+p.add_phone(PhoneNumber(21345678901, "Панда"))
+phones = p.get_phone_list()
+print(phones)
