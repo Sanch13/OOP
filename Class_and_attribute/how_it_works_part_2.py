7ff9777cd32d985e580ef18932ea008592fa9ff7
Aliaksandr Zubchyk <Sanch13sanch@gmail.com>
2022-08-11 15:32:08 +0300

    class_and_attribute_how_it_works

--- Class_and_attribute/how_it_works_part_2.py
+++ Class_and_attribute/how_it_works_part_2.py
@@ -238,6 +238,22 @@ cart.add(Notebook('Lenovo', 1500))   # Создание объекта Notebook 
 cart.add(Notebook('Dell', 2500))   # Создание объекта Notebook и добавление его в список объекта cart
 cart.add(Cup('Tea', 30))   # Создание объекта Cup и добавление его в список объекта cart
 #########################################################################################
+import sys
+class ListObject:
+    def __init__(self, data):
+        self.data = data
+        self.next_obj = None
 
+    def link(self, obj):
+        self.next_obj = obj
 
+lst_in = list(map(str.strip, sys.stdin.readlines()))
+
+head_obj = ListObject(lst_in[0])
+obj = head_obj
+for el in range(1, len(lst_in)):
+    new_obj = ListObject(lst_in[el])
+    obj.link(new_obj)
+    obj = new_obj
+#########################################################################################
 
