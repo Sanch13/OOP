df731758a2b039f785fc784cc342f4a885ec1105
Aliaksandr Zubchyk <Sanch13sanch@gmail.com>
2022-08-16 13:28:58 +0300

    static_class_methods

--- class_static_methods/tasks.py
+++ class_static_methods/tasks.py
@@ -22,10 +22,76 @@ class Loader:
         return seq  # Возврат списка
 
 res = Loader.parse_format("1, 2, 3, -5, 10", Factory)  # в переменную res вернется список из метода parse_format класса Loader
-print(res)
+# print(res)
 ########################################################################################################################
+from string import ascii_lowercase, digits  # из модуля string импортируем англ. алф. и цифры
+
+class TextInput:    # класс для ввода login
+    CHARS = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя " + ascii_lowercase  # формируем перем. CHARS рус. алф и анг. алф. + space
+    CHARS_CORRECT = CHARS + CHARS.upper() + digits  # формируем перем. CHARS_CORRECT рус. алф и анг. алф. + заглавные
+                                                    # буквы этих алфавитов + цифры
+
+    def __init__(self, name, size=10):  # инициал. лок. св-тв объектов класса TextInput
+        if self.check_name(name):   # проверка на коррект. ввода name
+            self.name = name    # инициал. лок. св-тва self.name
+            self.size = size    # инициал. лок. св-тва self.size
+
+    def get_html(self):  # возвращает сформированную HTML-строку в определенном формате
+        return f"<p class='login'>{self.name}: <input type='text' size={self.size} />"
+
+    @classmethod
+    def check_name(cls, name):   # проверка на коррект. ввода name
+        if 2 < len(name) < 51:   # Длина name от 3 до 50 символом включительно то True
+            if set(name) < set(cls.CHARS_CORRECT):   # name состоит из коректных символов то True
+                return True   # Вернет True
+
+        raise ValueError("некорректное поле name")  # Если хотя бы одно из условие if False то сгенерируется исключение
+
+class PasswordInput:
+    CHARS = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя " + ascii_lowercase  # формируем перем. CHARS рус. алф и анг. алф. + space
+    CHARS_CORRECT = CHARS + CHARS.upper() + digits  # формируем перем. CHARS_CORRECT рус. алф и анг. алф. + заглавные
+                                                    # буквы этих алфавитов + цифры
+
+    def __init__(self, name, size=10):  # инициал. лок. св-тв объектов класса PasswordInput
+        if self.check_name(name):   # проверка на коррект. ввода name
+            self.name = name    # инициал. лок. св-тва self.name
+            self.size = size    # инициал. лок. св-тва self.size
+
+    def get_html(self):  # возвращает сформированную HTML-строку в определенном формате
+        return f"<p class='password'>{self.name}: <input type='text' size={self.size} />"
+
+    @classmethod
+    def check_name(cls, name):   # проверка на коррект. ввода name
+        if 2 < len(name) < 51:   # Длина name от 3 до 50 символом включительно то True
+            if set(name) < set(cls.CHARS_CORRECT):   # name состоит из коректных символов то True
+                return True   # Вернет True
+
+        raise ValueError("некорректное поле name")  # Если хотя бы одно из условие if False то сгенерируется исключение
+
+class FormLogin:
+    def __init__(self, lgn, psw):  # инициал. лок. св-тв объектов класса FormLogin
+        self.login = lgn   # инициал. лок. св-тва self.login. lgn ссылка на экз. класса TextInput
+        self.password = psw   # инициал. лок. св-тва self.login. psw ссылка на экз. класса PasswordInput
+
+    def render_template(self):  # возвращает сформированную HTML-строку в определенном формате
+        return "\n".join(['<form action="#">', self.login.get_html(), self.password.get_html(), '</form>'])  # через
+    # лок. св-во self.login (ссылка на экз. класса TextInput) обратимся с методу get_html() класса TextInput. Он же
+    # в свою очередь обратится к лок. св-вам экз. класса TextInput и вернет их значения. Аналогично произойдет с
+    # self.password.get_html().
 
 
+login = FormLogin(TextInput("Логин"), PasswordInput("Пароль"))  # Сначала создается экз. класса TextInput. Перед
+    # созданеим лок. св-тв экз. класса идет проверка на коррект. ввода данных методом check_name(cls, name). Если метод
+    # вернет True то экз. класса создастся с переданным значение name. Аналогично далее с экз. класса PasswordInput.
+    # Далее создадуться лок. св-ва экз. класса FormLogin. Они будут ссылаться на объекты других классов. И так...
+    # login будет ссылаться на свои лок. св-ва, а его лок. св-ва будут ссылаться на объекты других классов.
+html = login.render_template()  # в переменную html будем возвращать html код с данными login и password. Для этого
+    # через экз. класса login обратимся с методу render_template(self), который в свою очередь обратиться к лок. св-вам
+    # self.login и self.login, а мы знаем что это ссылки на объекты других классов. Соответственно, он обратиться туда
+    # к методам get_html(), а они вернут данные имени (self.name = Логина) и данные пароля (self.name = password).
+    # Далее сформируется html код с данными. И они будут ссылать на переменную html. Всё!!!
+print(html)
+########################################################################################################################
 
 
 
