84be6ed27c13bc6e8c70a4923ce33bd51e8c7d29
Aliaksandr Zubchyk <Sanch13sanch@gmail.com>
2022-08-28 12:18:10 +0300

    Tasks_setattr__, __getattribute__, __getattr__,__delattr__

--- Маг. методы __setattr__, __getattribute__, __getattr__ и __delattr__/Task.py
+++ Маг. методы __setattr__, __getattribute__, __getattr__ и __delattr__/Task.py
@@ -282,14 +282,66 @@ class Circle:
         return False
 
 
-circle = Circle(10.5, 7, 22)
-print(circle.__dict__)
-circle.radius = -10 # прежнее значение не должно меняться, т.к. отрицательный радиус недопустим
-print(circle.__dict__)
-x, y = circle.x, circle.y
-res = circle.name # False, т.к. атрибут name не существует
-print(res)
+# circle = Circle(10.5, 7, 22)
+# print(circle.__dict__)
+# circle.radius = -10 # прежнее значение не должно меняться, т.к. отрицательный радиус недопустим
+# print(circle.__dict__)
+# x, y = circle.x, circle.y
+# res = circle.name # False, т.к. атрибут name не существует
+# print(res)
 ##################################################################################################
 
 
+class Dimensions:
+    MIN_DIMENSION = 9
+    MAX_DIMENSION = 1001
+
+    def __init__(self, a, b, c):
+        self.__a = a
+        self.__b = b
+        self.__c = c
+
+    @property
+    def a(self):
+        return self.__a
+
+    @a.setter
+    def a(self, value):
+        self.__a = value
+
+    @property
+    def b(self):
+        return self.__b
+
+    @b.setter
+    def b(self, value):
+        self.__b = value
+
+    @property
+    def c(self):
+        return self.__c
+
+    @c.setter
+    def c(self, value):
+        self.__c = value
+
+    @classmethod
+    def check_value(cls, value):
+        return cls.MIN_DIMENSION < value < cls.MAX_DIMENSION
+
+    def __setattr__(self, key, value):
+        if key in ('MIN_DIMENSION', 'MAX_DIMENSION'):
+            raise AttributeError("Менять атрибуты MIN_DIMENSION и MAX_DIMENSION запрещено.")
+        if self.check_value(value):
+            super().__setattr__(key, value)
+
+
+# d = Dimensions(10.5, 20.1, 30)
+# print(d.__dict__)
+# d.a = 8
+# d.b = 15
+# a, b, c = d.a, d.b, d.c  # a=10.5, b=15, c=30
+# print(d.__dict__)
+# d.MAX_DIMENSION = 10  # исключение AttributeError
+##################################################################################################
 
