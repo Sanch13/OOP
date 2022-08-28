3c703e9711c4d78d308a7a7b120ca377fd3b3941
Aliaksandr Zubchyk <Sanch13sanch@gmail.com>
2022-08-28 22:53:59 +0300

    Tasks_setattr__, __getattribute__, __getattr__,__delattr__

--- Маг. методы __setattr__, __getattribute__, __getattr__ и __delattr__/Task.py
+++ Маг. методы __setattr__, __getattribute__, __getattr__ и __delattr__/Task.py
@@ -6,13 +6,13 @@ class Book:
         self.year = year
 
     def __setattr__(self, key, value):  # Автомат. вызыв. при присваивании атрибуту нового знач.
-        if key in ('title', 'author') and not isinstance(value, str):   # проверяем условие
-            raise TypeError("Неверный тип присваиваемых данных.")   # вывод в консоль исключ.
+        if key in ('title', 'author') and not isinstance(value, str):  # проверяем условие
+            raise TypeError("Неверный тип присваиваемых данных.")  # вывод в консоль исключ.
 
-        if key in ('pages', 'year') and not isinstance(value, int):   # проверяем условие
-            raise TypeError("Неверный тип присваиваемых данных.")   # вывод в консоль исключ.
+        if key in ('pages', 'year') and not isinstance(value, int):  # проверяем условие
+            raise TypeError("Неверный тип присваиваемых данных.")  # вывод в консоль исключ.
 
-        super().__setattr__(key, value)    # вызов базового класса с передачей параметров
+        super().__setattr__(key, value)  # вызов базового класса с передачей параметров
 
 
 # book = Book('Python ООП', 'Сергей Балакирев', 123, 2022)
@@ -20,9 +20,9 @@ class Book:
 
 
 class Shop:
-    def __init__(self, name):   # Инициал. лок. св-в экз. кл. Shop
-        self.name = name        # Инициал. лок. св-в экз. кл. Shop.
-        self. goods = []        # У кажд. экз. кл..будет свой список
+    def __init__(self, name):  # Инициал. лок. св-в экз. кл. Shop
+        self.name = name  # Инициал. лок. св-в экз. кл. Shop.
+        self.goods = []  # У кажд. экз. кл..будет свой список
 
     def add_product(self, product):
         """Добавление нового товара в магазин (в конец списка goods)"""
@@ -37,27 +37,27 @@ class Shop:
 class Product:
     count_id = 0  # Атрибут кл. Product. Будем его использовать для счета экз. класса
 
-    def __new__(cls, *args, **kwargs):   # Переопр. маг. метод чтобы созд. уник. id экз. класса
+    def __new__(cls, *args, **kwargs):  # Переопр. маг. метод чтобы созд. уник. id экз. класса
         instance = super().__new__(cls)  # Созд. ссылку на нов. экз. класса
-        cls.count_id += 1                # Увел. атр. клс. на 1
-        instance.id = cls.count_id       # Через ссылку в экз. класса созд. атр. id и уст. знач.
+        cls.count_id += 1  # Увел. атр. клс. на 1
+        instance.id = cls.count_id  # Через ссылку в экз. класса созд. атр. id и уст. знач.
 
-        return instance                  # Вернем ссылку на базовый класс
+        return instance  # Вернем ссылку на базовый класс
 
     def __init__(self, name: str, weight: int, price: int):  # Инициал. лок. св-в экз. кл. Product
-        self.name = name                                     # Инициал. лок. св-в экз. кл. Product
-        self.weight = weight                                 # Инициал. лок. св-в экз. кл. Product
-        self.price = price                                   # Инициал. лок. св-в экз. кл. Product
+        self.name = name  # Инициал. лок. св-в экз. кл. Product
+        self.weight = weight  # Инициал. лок. св-в экз. кл. Product
+        self.price = price  # Инициал. лок. св-в экз. кл. Product
 
     def __setattr__(self, key, value):  # Автомат. вызыв. при присваивании атрибуту нового знач.
-        if key == 'name' and not isinstance(value, str):   # проверяем условие
-            raise TypeError("Неверный тип присваиваемых данных.")   # вывод в консоль исключ.
+        if key == 'name' and not isinstance(value, str):  # проверяем условие
+            raise TypeError("Неверный тип присваиваемых данных.")  # вывод в консоль исключ.
 
         if key in ('weight', 'price'):  # проверяем условие, что key явл. одним из знач.
             if isinstance(value, (int, float)) and value <= 0:  # проверяем условие
-                raise TypeError("Неверный тип присваиваемых данных.")   # вывод в консоль исключ.
+                raise TypeError("Неверный тип присваиваемых данных.")  # вывод в консоль исключ.
 
-        super().__setattr__(key, value)    # вызов базового класса с передачей параметров
+        super().__setattr__(key, value)  # вызов базового класса с передачей параметров
 
     def __delattr__(self, item):  # Автомат. вызыв. при удалении атрибута item
         """Метод __delattr__ вызывается в момент удаления какого-либо атрибута из экземпляра
@@ -149,49 +149,49 @@ class Course:
 # module_1.remove_lesson(1)
 # print(module_1.__dict__)
 ##################################################################################################
-
-
-class Picture:
-    def __init__(self, name, author, descr):
-        self.name = name
-        self.author = author
-        self.descr = descr
-
-
-class Mummies:
-    def __init__(self, name, author, descr):
-        self.name = name
-        self.author = author
-        self.descr = descr
-
-
-class Papyri:
-    def __init__(self, name, date, descr):
-        self.name = name
-        self.date = date
-        self.descr = descr
-
-
-class Museum:
-    def __init__(self, name):
-        self.name = name
-        self.exhibits = []
-
-    def add_exhibit(self, obj):
-        self.exhibits.append(obj)
-
-    def remove_exhibit(self, obj):
-        self.exhibits.remove(obj)
-
-    def get_info_exhibit(self, indx):
-        return f"Описание экспоната {self.exhibits[indx].name}: {self.exhibits[indx].descr}"
-
-
-mus = Museum("Эрмитаж")
-mus.add_exhibit(Picture("Балакирев с подписчиками пишет письмо иноземному султану", "Неизвестный автор", "Вдохновляющая, устрашающая, волнующая картина"))
-mus.add_exhibit(Mummies("Балакирев", "Древняя Россия", "Просветитель XXI века, удостоенный мумификации"))
-p = Papyri("Ученья для, не злата ради", "Древняя Россия", "Самое древнее найденное рукописное свидетельство о языках программирования")
-mus.add_exhibit(p)
+#
+#
+# class Picture:
+#     def __init__(self, name, author, descr):
+#         self.name = name
+#         self.author = author
+#         self.descr = descr
+#
+#
+# class Mummies:
+#     def __init__(self, name, author, descr):
+#         self.name = name
+#         self.author = author
+#         self.descr = descr
+#
+#
+# class Papyri:
+#     def __init__(self, name, date, descr):
+#         self.name = name
+#         self.date = date
+#         self.descr = descr
+#
+#
+# class Museum:
+#     def __init__(self, name):
+#         self.name = name
+#         self.exhibits = []
+#
+#     def add_exhibit(self, obj):
+#         self.exhibits.append(obj)
+#
+#     def remove_exhibit(self, obj):
+#         self.exhibits.remove(obj)
+#
+#     def get_info_exhibit(self, indx):
+#         return f"Описание экспоната {self.exhibits[indx].name}: {self.exhibits[indx].descr}"
+#
+#
+# mus = Museum("Эрмитаж")
+# mus.add_exhibit(Picture("Балакирев с подписчиками пишет письмо иноземному султану", "Неизвестный автор", "Вдохновляющая, устрашающая, волнующая картина"))
+# mus.add_exhibit(Mummies("Балакирев", "Древняя Россия", "Просветитель XXI века, удостоенный мумификации"))
+# p = Papyri("Ученья для, не злата ради", "Древняя Россия", "Самое древнее найденное рукописное свидетельство о языках программирования")
+# mus.add_exhibit(p)
 # print(mus.__dict__)
 # for x in mus.exhibits:
 #     print(x.descr)
@@ -274,7 +274,7 @@ class Circle:
 
     def __setattr__(self, key, value):  # проверяем value явл. (int, float) и больше 0
         if isinstance(value, (int, float)) and value > 0:
-            super().__setattr__(key, value)   # вызов базового класса с передачей параметров
+            super().__setattr__(key, value)  # вызов базового класса с передачей параметров
         elif isinstance(value, str):  # проверяем value явл. (str). Вызыв. исключение
             raise TypeError("Неверный тип присваиваемых данных.")
 
@@ -343,5 +343,77 @@ class Dimensions:
 # a, b, c = d.a, d.b, d.c  # a=10.5, b=15, c=30
 # print(d.__dict__)
 # d.MAX_DIMENSION = 10  # исключение AttributeError
+##################################################################################################
+
+
+import time  # Импортируем модуль time для взятия кол-ва числа секунд от начала эпохи
+
+
+class Filter:                            # Общий (базовый) класс для Mechanical, Aragon, Calcium
+    def __init__(self, date: float):     # Общий инициал. для Mechanical, Aragon, Calcium
+        self.date = date
+
+    def __setattr__(self, key, value):   # Общий __setattr__. для Mechanical, Aragon, Calcium
+        if key not in self.__dict__:
+            self.__dict__[key] = value
+
+
+class Mechanical(Filter):
+    pass
+
+
+class Aragon(Filter):
+    pass
+
+
+class Calcium(Filter):
+    pass
+
+
+class GeyserClassic:
+    MIN_DATE_FILTER = 0      # атрибут класса GeyserClassic. Мин. критерий
+    MAX_DATE_FILTER = 100    # атрибут класса GeyserClassic. Мах. критерий
+    attr = {1: Mechanical, 2: Aragon, 3: Calcium}    # атрибут класса GeyserClassic.
+
+    def __init__(self):      # Инициал. лок. св-ва экз. класса GeyserClassic
+        self.slots = {1: None, 2: None, 3: None}
+
+    @classmethod
+    def check_service_life(cls, obj) -> bool:      # Метод вернет True если усл. в диапазоне
+        return cls.MIN_DATE_FILTER <= time.time() - obj.date <= cls.MAX_DATE_FILTER
+
+    def add_filter(self, slot_num, filter):  # Метод добавит объект если его нет и он соотв.
+        if isinstance(filter, self.attr.get(slot_num)) and not self.slots.get(slot_num):
+            self.slots[slot_num] = filter
+
+    def remove_filter(self, slot_num):  # Метод удал. объект по индексу
+        self.slots[slot_num] = None
+
+    def get_filters(self):  # Метод возващ. кортеж объектов фильтра
+        return tuple(self.slots.values())
+
+    def water_on(self) -> bool:  # Вернет True если условия True))
+        if None in self.slots.values():  # Если есть хотя бы один None return False
+            return False
+        return len(list(filter(self.check_service_life, self.get_filters()))) == 3
+        # Проходим по кортежу объектов. Каждый объект попадет в check_service_life. Она вернет
+        # True если объект в нужном диапазоне.Если кол-во True == 3, то возвращаем True
+
+# my_water = GeyserClassic()
+# my_water.add_filter(1, Mechanical(time.time()))
+# my_water.add_filter(2, Aragon(time.time()))
+# print(my_water.slots)
+# w = my_water.water_on() # False
+# print(w)
+# my_water.add_filter(3, Calcium(time.time()))
+# print(my_water.slots)
+# w = my_water.water_on() # True
+# print(w)
+# my_water.remove_filter(1)
+# print(my_water.slots)
+# my_water.add_filter(1, Mechanical(time.time() - 99))
+# print(my_water.slots)
+# w = my_water.water_on() # False
+# print(w)
 ##################################################################################################
 
