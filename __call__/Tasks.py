e723a32ed6e99312189e5bd6229720845812cd86
Aliaksandr Zubchyk <Sanch13sanch@gmail.com>
2022-08-29 21:28:53 +0300

    Tasks__call__

--- __call__/Tasks.py
+++ __call__/Tasks.py
@@ -85,3 +85,19 @@ class CharsValidator:
 ###################################################################################################
 
 
+class DigitRetrieve:
+    def __call__(self, *args, **kwargs):
+        try:
+            return int(args[0])
+        except ValueError:
+            return None
+
+
+dg = DigitRetrieve()
+st = ["123", "abc", "-56.4", "0", "-5"]
+digits_1 = list(map(dg, st))  # [123, None, None, 0, -5]
+print(digits_1)
+
+
+
+
