4fa0f6111a46919f2e0e5aec7a09b06e317cc093
Aliaksandr Zubchyk <Sanch13sanch@gmail.com>
2022-08-29 15:28:54 +0300

    Tasks__call__

--- __call__/Tasks.py
+++ __call__/Tasks.py
@@ -43,4 +43,45 @@ class ImageFileAcceptor:
 # print(list(image_filenames))  # ["boat.jpg", "ava.jpg", "forest.jpeg"
 ###################################################################################################
 
+from string import ascii_lowercase, digits
+
+class LoginForm:
+    def __init__(self, name, validators=None):
+        self.name = name
+        self.validators = validators
+        self.login = ""
+        self.password = ""
+
+    def post(self, request):
+        self.login = request.get('login', "")
+        self.password = request.get('password', "")
+
+    def is_validate(self):
+        if not self.validators:
+            return True
+
+        for v in self.validators:
+            if not v(self.login) or not v(self.password):
+                return False
+
+        return True
+
+
+class LengthValidator:
+    def __init__(self, min_length, max_length):
+        self.min_length = min_length
+        self.max_length = max_length
+
+    def __call__(self, *args, **kwargs):    # изм. поведение для функторов экз. класса
+        return self.min_length <= len(args[0]) <= self.max_length
+
+
+class CharsValidator:
+    def __init__(self, chars):
+        self.chars = chars
+
+    def __call__(self, *args, **kwargs):    # изм. поведение для функторов экз. класса
+        return set(args[0]) <= set(self.chars)
+###################################################################################################
+
 
