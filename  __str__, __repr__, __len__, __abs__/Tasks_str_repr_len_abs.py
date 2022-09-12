a47b0d69e995ea9a7e58a4dd631907b6b646148e
Aliaksandr Zubchyk <Sanch13sanch@gmail.com>
2022-09-12 13:12:36 +0300

    Tasks_str_repr_len_abs

---  __str__, __repr__, __len__, __abs__/Tasks_str_repr_len_abs.py
+++  __str__, __repr__, __len__, __abs__/Tasks_str_repr_len_abs.py
@@ -36,41 +36,135 @@
 # model_1= Model()
 # print(model_1)
 ###################################################################################################
-class WordString:
-    def __init__(self, string=''):
-        self.string = string
-
-    def __call__(self, indx):
-        return self.words(indx)
-
-    def __len__(self):
-        """Позволяет применять функцию len() к экземплярам класса"""
-        return len(self.__list)
+# class Model:
+#     def query(self, **kwargs):
+#         self.__dict__ = kwargs
+#
+#     def __str__(self):
+#         if len(self.__dict__) == 0:
+#             return f"Model"
+#         return f'Model: ' + ', '.join(f'{k} = {v}' for k, v in self.__dict__.items())
+#
+#
+# model = Model()
+# model.query(id=1, fio='Sergey', old=33)
+# print(model)
+# model_1 = Model()
+# print(model_1)
+###################################################################################################
+# class WordString:
+#     def __init__(self, string=''):
+#         self.string = string
+#
+#     def __call__(self, indx):
+#         return self.words(indx)
+#
+#     def __len__(self):
+#         """Позволяет применять функцию len() к экземплярам класса"""
+#         return len(self.__list)
+#
+#     @property
+#     def string(self):
+#         return self.__string
+#
+#     @string.setter
+#     def string(self, value):
+#         self.__string = value
+#         self.__list = value.split()
+#
+#     def words(self, indx):
+#         """должно возвращаться слово по его индексу
+#         (indx - порядковый номер слова в строке, начиная с 0)."""
+#         return self.__list[indx]
+#
+#
+# words = WordString()
+# words.string = "Курс по Python ООП"
+# n = len(words)
+# first = "" if n == 0 else words(0)
+# print(words.string)
+# print(f"Число слов: {n}; первое слово: {first}")
+###################################################################################################
+class ObjList:
+    def __init__(self, data: str):
+        self.data = data
+        self.prev = self.next = None
 
     @property
-    def string(self):
-        return self.__string
+    def data(self):
+        return self.__data
 
-    @string.setter
-    def string(self, value):
-        self.__string = value
-        self.__list = value.split()
+    @data.setter
+    def data(self, value):
+        self.__data = value
 
-    def words(self, indx):
-        """должно возвращаться слово по его индексу
-        (indx - порядковый номер слова в строке, начиная с 0)."""
-        return self.__list[indx]
+    @property
+    def prev(self):
+        return self.__prev
+
+    @prev.setter
+    def prev(self, value):
+        self.__prev = value
+
+    @property
+    def next(self):
+        return self.__next
+
+    @next.setter
+    def next(self, value):
+        self.__next = value
 
 
-words = WordString()
-words.string = "Курс по Python ООП"
-n = len(words)
-first = "" if n == 0 else words(0)
-print(words.string)
-print(f"Число слов: {n}; первое слово: {first}")
+class LinkedList:
+    def __init__(self):
+        self.head = self.tail = None
+
+    def add_obj(self, obj):
+        if self.head is None:
+            self.head = self.tail = obj
+            return
+        self.tail.next = obj
+        obj.prev = self.tail
+        self.tail = obj
+
+    def remove_obj(self, indx):
+        pointer = self.head
+        if indx == 0:
+            if pointer is None:
+                return
+            if pointer.next is None:
+                self.head = self.tail = None
+                return
+            pointer.next.prev = None
+            self.head = pointer.next
+            return
+        count = 0
+        # while pointer:
+        #     if count == indx:
+        #
+        #     pointer = pointer.next
+        #     count += 1
 
 
 
+
+    def show_data(self):
+        out_data = []
+        pointer = self.head
+        while pointer:
+            out_data.append(pointer.data)
+            pointer = pointer.next
+
+        return out_data
+
+
+linked_lst = LinkedList()
+linked_lst.add_obj(ObjList("Sergey"))
+linked_lst.add_obj(ObjList("Balakirev"))
+print(linked_lst.show_data())
+linked_lst.remove_obj(0)
+linked_lst.add_obj(ObjList("Python"))
+print(linked_lst.show_data())
 
 
 
