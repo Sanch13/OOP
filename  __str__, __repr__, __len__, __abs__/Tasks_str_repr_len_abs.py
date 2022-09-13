41858843ac7ee74812f4a9be145398ee02407d88
Aliaksandr Zubchyk <Sanch13sanch@gmail.com>
2022-09-13 09:47:44 +0300

    Tasks_str_repr_len_abs

---  __str__, __repr__, __len__, __abs__/Tasks_str_repr_len_abs.py
+++  __str__, __repr__, __len__, __abs__/Tasks_str_repr_len_abs.py
@@ -215,6 +215,43 @@
 # ln.add_obj(ObjList("Python"))
 # assert ln(2) == "Python", "неверное значение атрибута __data, взятое по индексу"
 ###################################################################################################
-
+# class Complex:
+#     def __init__(self, real, img):
+#         self.real = real
+#         self.img = img
+#
+#     @property
+#     def real(self):
+#         return self.__real
+#
+#     @real.setter
+#     def real(self, value):
+#         self.__check_value(value)
+#         self.__real = value
+#
+#     @property
+#     def img(self):
+#         return self.__img
+#
+#     @img.setter
+#     def img(self, value):
+#         self.__check_value(value)
+#         self.__img = value
+#
+#     @staticmethod
+#     def __check_value(value):
+#         if not isinstance(value, (int, float)):
+#             raise ValueError("Неверный тип данных.")
+#
+#     def __abs__(self):
+#         return (self.real ** 2 + self.img ** 2) ** 0.5
+#
+#
+# cmp = Complex(7, 8)
+# cmp.real = 3
+# cmp.img = 4
+# c_abs = abs(cmp)
+# print(c_abs)
+###################################################################################################
 
 
