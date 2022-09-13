88eb7ec7244476cf33b062d04c5c287034051a4c
Aliaksandr Zubchyk <Sanch13sanch@gmail.com>
2022-09-13 13:40:35 +0300

    Tasks_str_repr_len_abs

---  __str__, __repr__, __len__, __abs__/Tasks_str_repr_len_abs.py
+++  __str__, __repr__, __len__, __abs__/Tasks_str_repr_len_abs.py
@@ -253,5 +253,41 @@
 # c_abs = abs(cmp)
 # print(c_abs)
 ###################################################################################################
+# class RadiusVector:
+#     def __init__(self, *args):
+#         if len(args) == 1:
+#             self.__coords = [0] * args[0]
+#         else:
+#             self.__coords = list(args)
+#
+#     def set_coords(self, *args):
+#         total_elem = min(len(args), len(self.__coords))
+#         self.__coords[:total_elem] = args[:total_elem]
+#
+#     def get_coords(self):
+#         return tuple(self.__coords)
+#
+#     def __len__(self):
+#         return len(self.__coords)
+#
+#     def __abs__(self):
+#         return sum(x ** 2 for x in self.__coords) ** 0.5
+#
+#
+# vector3D = RadiusVector(3)
+# print(vector3D.__dict__)
+# vector3D.set_coords(3, -5.6, 8)
+# print(vector3D.__dict__)
+# a, b, c = vector3D.get_coords()
+# vector3D.set_coords(8, -156.6, 4, 10, 11) # ошибки быть не должно, последние две координаты
+# # игнорируются
+# print(vector3D.__dict__)
+# vector3D.set_coords(1, 2) # ошибки быть не должно, меняются только первые две координаты
+# print(vector3D.__dict__)
+# res_len = len(vector3D) # res_len = 3
+# print(res_len)
+# res_abs = abs(vector3D)
+# print(res_abs)
+###################################################################################################
 
 
