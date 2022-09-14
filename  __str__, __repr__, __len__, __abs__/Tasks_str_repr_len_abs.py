92a84a971dd07981e7b04fdf7c02c34cab2e72cf
Aliaksandr Zubchyk <Sanch13sanch@gmail.com>
2022-09-14 11:35:54 +0300

    Tasks_str_repr_len_abs_

---  __str__, __repr__, __len__, __abs__/Tasks_str_repr_len_abs.py
+++  __str__, __repr__, __len__, __abs__/Tasks_str_repr_len_abs.py
@@ -368,6 +368,26 @@
 # n = len(recipe) # n = 3
 # print(n)
 ###################################################################################################
-
-
-
+# class PolyLine:
+#     def __init__(self, *args):
+#         self.coords = list(args)
+#
+#     def add_coord(self, x, y):
+#         self.coords.append((x, y))
+#
+#     def remove_coord(self, indx):
+#         if indx < len(self.coords):
+#             self.coords.pop(indx)
+#
+#     def get_coords(self):
+#         return self.coords
+#
+#
+# poly = PolyLine((1, 2), (3, 5), (0, 10), (-1, 8))
+# print(poly.__dict__)
+# poly.add_coord(22, 34)
+# print(poly.__dict__)
+# poly.remove_coord(4)
+# print(poly.__dict__)
+# print(poly.get_coords())
+###################################################################################################
