91f7d6724198b87f14cca38334ddc00a3349152a
sanch13 <sanch13sanch@gmail.com>
2023-07-31 08:40:54 +0300

    description of the work add, sub, mul, truediv

--- Magic_methods__add__sub__mul__truediv/How_it_works__add__sub__mul__truediv.py
+++ Magic_methods__add__sub__mul__truediv/How_it_works__add__sub__mul__truediv.py
@@ -14,26 +14,55 @@
 # x % y    __mod__(self, other)     x %= y   __imod__(self, other)
 
 class Clock:
-	__DAY = 86400  # число секунд в одном дне
+    __DAY = 86400  # число секунд в одном дне
 
-	def __init__(self, seconds: int):
-		if not isinstance(seconds, int):
-			raise TypeError("Секунды должны быть целым числом")
-		self.seconds = seconds % self.__DAY
+    def __init__(self, seconds: int):
+        if not isinstance(seconds, int):
+            raise TypeError("Секунды должны быть целым числом")
+        self.seconds = seconds % self.__DAY
 
-	def get_time(self):
-		s = self.seconds % 60  # секунды
-		m = (self.seconds // 60) % 60  # минуты
-		h = (self.seconds // 3600) % 24  # часы
-		return f"{h:02}:{m:02}:{s:02}"
+    def get_time(self):
+        s = self.seconds % 60  # секунды
+        m = (self.seconds // 60) % 60  # минуты
+        h = (self.seconds // 3600) % 24  # часы
+        return f"{h:02}:{m:02}:{s:02}"
 
-	def __sub__(self, other):
-		return Clock(self.seconds - other)
+    def __add__(self, other):
+        """Складывает лок. атриб. экз. класса Clock и созд. новый экз. с суммой"""
+        if not isinstance(other, (int, Clock)):
+            raise ArithmeticError("Правый операнд должен быть типом int или объектом Clock")
 
+        # Теперь, в программе можно складывать и отдельные целые числа и объекты классов Clock
+        sc = other if isinstance(other, int) else other.seconds
+        return Clock(self.seconds + sc)
 
-c1 = Clock(3000)
-print(c1.seconds)
-c2 = Clock(1000)
+    def __radd__(self, other):
+        """сложения текущего объекта класса Clock с параметром other, который может быть или
+        числом или тоже объектом класса Clock. В свою очередь будет вызван метод __add__,
+        но с правильным порядком типов данных, поэтому сложение пройдет без ошибок"""
+        return self + other
 
-c3 = c1 - 2500
-print(c3.get_time())
+    def __iadd__(self, other):
+        """не создаем нового объекта, а меняем число секунд в текущем объекте."""
+        if not isinstance(other, (int, Clock)):
+            raise ArithmeticError("Правый операнд должен быть типом int или объектом Clock")
+
+        sc = other if isinstance(other, int) else other.seconds
+        self.seconds += sc
+
+        return self
+
+# c1 = Clock(1000)
+# c1.seconds = c1.seconds + 100
+# c1 = c1 + 100
+# с1 = c1.__add__(100)  Этот объект возвращается методом __add__ и переменная c1
+# начинает ссылаться на этот новый экземпляр класса. 1000 + 100 = 1100, с1 = 1100
+# c1 = Clock(1000)
+# c2 = Clock(2000)
+# c3 = Clock(3000)
+# c4 = c1 + c2 + c3
+# метод __add__ возвращает каждый раз новый экземпляр класса Clock.
+# c1 = 100 + c1 # 100.__add__(c1) # __radd__()
+# c1 += 100 # в методе __add__ создается новый объект класса Clock,
+# тогда как при операции += этого делать не обязательно. __iadd__ не создаем нового объекта,
+# а меняет число секунд в текущем
